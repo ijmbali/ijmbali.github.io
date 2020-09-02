06f201575d71c20529a98c8a589d2ca29c5ae924
bcmediac15 <made@artha.web.id>
2020-09-02 12:16:18 +0800

    AwalPersiapan
    Signed-off-by: bcmediac15 <made@artha.web.id>

---
+++ index.php
@@ -0,0 +1,246 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">  
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style-min.css">
+    <title>ISTI Jaya Mandiri Bali</title>
+    
+  </head>
+  <body>
+    
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-10">
+                    
+                </div>
+            </div> 
+        </div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <a class="navbar-brand" href="#">IJM Bali</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#home">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#about">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#portfolio">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact</a>
+                  </li>
+              </ul>
+            </div>
+          </nav>
+    </header>
+    
+    <a name="home">
+        <section id="home" class="home">
+            <div class="jumbotron jumbotron-fluid">
+                <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="img/slideshow/01.jpg" alt="First slide">
+      <div class="carousel-caption d-md-block ftco-animate">
+        <h2>Test Test</h2>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/slideshow/02.jpg" alt="Second slide">
+    </div>
+  </div>
+</div>
+                
+            </div>
+        </section>
+    </a>
+
+    <a name="about">
+        <section id="abour" class="about">
+            <div class="container mb-5">
+                <div class="row text-center ftco-animate">
+                    <div class="col">
+                        <h3>About</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col text-center ftco-animate">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo, sed! Ut exercitationem fugiat eum! Ipsa dolorum quod illo voluptates nostrum.
+                    </div>
+                    <div class="col text-center ftco-animate">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore natus totam quis dolorum expedita rem rerum tempora deleniti eaque atque!
+                    </div>
+                </div>
+            </div>
+        </section>
+    </a>
+    <a name="portfolio">
+        <section id="portofolio" class="portofolio">
+            <div class="container text-center ftco-animate">
+                <div class="row">
+                    <div class="col">
+                        <h3>Portfolio</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="container mb-5">
+                <div class="row mb-3">
+                    <div class="col-md">
+                        <div class="card">
+                            <img class="card-img-top ftco-animate" src="img/thumbs/1.png" alt="Gambar 1">
+                            <div class="card-body ftco-animate">
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <div class="card">
+                            <img class="card-img-top ftco-animate" src="img/thumbs/2.png" alt="Gambar 1">
+                            <div class="card-body ftco-animate">
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <div class="card">
+                            <img class="card-img-top ftco-animate" src="img/thumbs/3.png" alt="Gambar 1">
+                            <div class="card-body ftco-animate">
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md">
+                        <div class="card">
+                            <img class="card-img-top" src="img/thumbs/4.png" alt="Gambar 1">
+                            <div class="card-body">
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <div class="card">
+                            <img class="card-img-top" src="img/thumbs/5.png" alt="Gambar 1">
+                            <div class="card-body">
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <div class="card">
+                            <img class="card-img-top" src="img/thumbs/6.png" alt="Gambar 1">
+                            <div class="card-body">
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </a>
+    <a name="contact">
+        <section id="contact" class="contact ftco-section ftco-no-pb ftco-partner">
+            <div class="container mb-5">
+                <div class="row pt-4 mb-4">
+                    <div class="col text-center ftco-animate">
+                        <h3>Contact Us</h3>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-lg-4">
+                        <div class="card">
+                            <div class="card-body text-white bg-primary mb-3 text-center ftco-animate">
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                        <ul class="list-group">
+                            <li class="list-group-item ftco-animate">Alamat</li>
+                            <li class="list-group-item ftco-animate">Bali Kencana Resort I Blok C15</li>
+                            <li class="list-group-item ftco-animate">Angas Sari, Ungasan</li>
+                            <li class="list-group-item ftco-animate">Kuta Selatan Badung Bali</li>
+                          </ul>
+                    </div>
+                    <div class="col-lg-6">
+                        <form>
+                            <div class="form-group ftco-animate">
+                              <label for="email">Email address</label>
+                              <input type="email" class="form-control" id="email">
+                            </div>
+                            <div class="form-group ftco-animate">
+                              <label for="nama">Nama</label>
+                              <input type="text" class="form-control" id="nama">
+                            </div>
+                            <div class="form-group ftco-animate">
+                              <label for="exampleFormControlTextarea1">Example textarea</label>
+                              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            </div>
+                            <div class="form-group ftco-animate">
+                                <button type="button" class="btn btn-primary">Send</button>
+                            </div>
+                          </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </a>
+    <footer>
+        <section id="footer" class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col text-white text-center">
+                        <p>&copy;2020</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </footer>
+    <navbottom>
+        <section id="navbtton" class="navbtton">
+            <ul id="menu">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#portofolio">Portofolio</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </section>
+    </navbottom>
+
+    <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/scrollax.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/main.js"></script>
+
+  </body>
+</html>
